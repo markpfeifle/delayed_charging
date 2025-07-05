e7ab3ca347d6d3b312c29a49d2900f2c841cf1f3
markpfeifle <40261627+markpfeifle@users.noreply.github.com>
2025-07-05 23:32:17 +0200

    Create sensor.py

---
+++ sensor.py
@@ -0,0 +1,48 @@
+import logging
+from datetime import timedelta
+
+from homeassistant.components.sensor import (
+    SensorEntity,
+    SensorDeviceClass,
+)
+
+from custom_components.delayed_charging_start.service import get_delayed_charging_start
+
+_LOGGER = logging.getLogger(__name__)
+
+DEFAULT_NAME = "Delayed Charging Start"
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up the sensor platform asynchronously."""
+    async_add_entities([DelayedChargingStart()])
+
+
+class DelayedChargingStart(SensorEntity):
+
+    def __init__(self):
+        """Initialize the sensor."""
+        self._state = None
+        self._name = DEFAULT_NAME
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def native_value(self):
+        return self._state
+
+    @property
+    def device_class(self):
+        return SensorDeviceClass.TIMESTAMP
+
+    @property
+    def state_class(self):
+        return None
+
+    async def async_update(self):
+        try:
+            self._state = await get_delayed_charging_start()
+        except Exception as e:
+            _LOGGER.error("Error fetching delayed charging start: %s", e)
+            self._state = None
